5b74b4f9cbabba0fa31a8f471fb7863abb27b8ed
Will <wladerer@wladerer.wladerer>
2025-11-21 16:05:43 -0800

    made slab generation more organized and aggressive

--- src/tinykit/slabgen.py
+++ src/tinykit/slabgen.py
@@ -1,13 +1,15 @@
 import argparse
 import warnings
+import json
+import numpy as np
 from surfaxe.generation import generate_slabs
 
 from pymatgen.io.vasp.inputs import Incar, Kpoints, Poscar, Potcar, VaspInput
 from pymatgen.core.structure import Structure
-from pymatgen.core.surface import SlabGenerator
+from pymatgen.core.surface import Slab, generate_all_slabs
 from pathlib import Path
 
-kpoints = Kpoints.gamma_automatic((5,5,1), shift=(0,0,0))
+kpoints = Kpoints.gamma_automatic((1,1,1), shift=(0,0,0))
 
 incar_dict = {
     "ALGO": "Normal",
@@ -21,7 +23,6 @@ incar_dict = {
     "ISYM": 2,
     "IVDW": 4,
     "IWAVPR": 1,
-    "KPAR": 8,
     "LASPH": True,
     "LCHARG": True,
     "LMAXMIX": 6,
@@ -37,41 +38,50 @@ incar_dict = {
     "NCORE": 64,
 }
 
-# Filter warnings
-warnings.filterwarnings("always")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
-warnings.filterwarnings("ignore", message="POTCAR data")
-warnings.filterwarnings("ignore", message="Overriding the POTCAR functional")
 
-def generate_miller_planes(hkl_max: int) -> list[tuple[int]]:
-    """generates a list of miller planes for slab generation, including negative planes"""
-    
-    miller_planes = []
-    for h in range(-hkl_max, hkl_max + 1):
-        for k in range(-hkl_max, hkl_max + 1):
-            for l in range(-hkl_max, hkl_max + 1):
-                if h == 0 and k == 0 and l == 0:
-                    continue
-                miller_planes.append((h, k, l))
-    
-    return miller_planes
 
-def write_directories(structures: list[Structure], directory: str) -> None:
-    """writes each structure to its own directory"""
 
-    for index, structure in enumerate(structures):
-        path = Path(directory) / f"slab_{index}"
+def write_slab_directories(
+    slabs: list[Slab], 
+    directory: str, 
+    min_slab_size: float,
+) -> None:
+
+    seen = set()
+    root = Path(directory)
+
+    for termination_index, slab in enumerate(slabs):
+
+        slab = slab.get_sorted_structure(key=lambda s: s.species_string)
+
+        sym_dir = "sym" if slab.is_symmetric() else "asym"
+
+        hkl = "".join(str(i) for i in slab.miller_index)
+        size_str = f"{min_slab_size:.2f}"
+
+        path = (
+            Path(directory)
+            / hkl
+            / size_str
+            / sym_dir
+            / f"term_{termination_index}"
+        )
         path.mkdir(parents=True, exist_ok=True)
 
-        #create poscar from structure
-        poscar = Poscar(structure)
+        poscar = Poscar(slab)
         potcar = Potcar(symbols=poscar.site_symbols, functional="PBE")
         incar = Incar.from_dict(incar_dict)
-        
-        input_set = VaspInput(incar=incar, kpoints=kpoints, poscar=poscar, potcar=potcar)
+
+        input_set = VaspInput(
+            incar=incar,
+            kpoints=kpoints,
+            poscar=poscar,
+            potcar=potcar,
+        )
         input_set.write_input(path)
 
-    return None
+        with open(path / "slab.json", "w") as f:
+            json.dump(slab.as_dict(), f, indent=2)
 
 # Define argument parser
 def parse_args():
@@ -81,18 +91,15 @@ def parse_args():
     parser.add_argument('structure', type=str,
                         help='Path to the structure file')
     parser.add_argument('--hkl', type=int, default=1,
-                        help='Miller indices for the surface (default: max index 1)')
+                        help='Max Miller index for the surface (default: 1)')
     parser.add_argument('-t','--thicknesses', type=float, nargs='+', default=[12],
                         help='Slab thicknesses to generate (default: [12])')
-    parser.add_argument('--vacuums', type=float, nargs='+', default=[15],
+    parser.add_argument('--vacuum', type=float, default=15.0,
                         help='Vacuum thicknesses to add (default: [15])')
     parser.add_argument('--layers_to_relax', type=int, default=3,
                         help='Number of layers to relax (default: 3)')
-    parser.add_argument('--config_dict', type=str, default='pe_relax',
-                        help='Configuration dictionary for slab generation (default: pe_relax)')
-    parser.add_argument('-a', '--allow-asymmetric', action='store_true')
-    parser.add_argument('--no-tasker', action='store_true',
-                        help='Uses pymatgen to generate slabs without checking for suitability (dipole, symmetry, etc)')
+    parser.add_argument('--symmetrize', action='store_true', default=False, help='Force top and bottom surface to be equivalent (does not preserve stoichiometry)')
+    parser.add_argument('-d', "--directory",default='GeneratedSlabs', help='parent directory of all slabs',type=str)
     
     return parser.parse_args()
 
@@ -100,42 +107,40 @@ def main():
     # Parse arguments
     args = parse_args()
 
-    if args.no_tasker:
-        # Generate slabs without tasker
-        structure = Structure.from_file(args.structure)
-        slabs_dict = {}
-        for hkl in generate_miller_planes(args.hkl):
-            slabgen = SlabGenerator(
-                structure,
-                hkl,
-                min_slab_size=min(args.thicknesses),
-                min_vacuum_size=15,
-                center_slab=True,
-                reorient_lattice=True,
-                lll_reduce=True,
-            )
-            slabs = slabgen.get_slabs()
-            #hkl to string
-            hkl_str = f"{hkl[0]}{hkl[1]}{hkl[2]}"
-            slabs_dict[hkl_str] = slabs
+    # Generate slabs without tasker
+    structure = Structure.from_file(args.structure)
+    total_generated_slabs = 0
+    for thickness in args.thicknesses:
 
-            write_directories(slabs, f"GeneratedSlabs/{hkl_str}") 
-
-    else:
-        # Generate slabs
-
-        slabs_dict = generate_slabs(
-            structure=args.structure,
-            hkl=args.hkl,
-            thicknesses=args.thicknesses,
-            vacuums=args.vacuums,
-            make_input_files=True,
-            layers_to_relax=args.layers_to_relax,
-            save_slabs=True,
-            config_dict=args.config_dict,
-            is_symmetric=False if args.allow_asymmetric else True
+        slabs_asym = generate_all_slabs(
+            structure,
+            max_index=args.hkl,
+            min_slab_size=thickness,
+            min_vacuum_size=args.vacuum,
+            symmetrize=False,
+            lll_reduce=True,
+            center_slab=True,
+            primitive=True,
         )
+    
+        slabs_sym = generate_all_slabs(
+            structure,
+            max_index=args.hkl,
+            min_slab_size=thickness,
+            min_vacuum_size=args.vacuum,
+            symmetrize=True,
+            lll_reduce=True,
+            center_slab=True,
+            primitive=True,
+        )
+    
+        slabs = slabs_asym + slabs_sym
+#        slabs = remove_duplicates(slabs)  # your existing hash-based system
+    
+        total_generated_slabs += len(slabs)
+        write_slab_directories(slabs, args.directory, min_slab_size=thickness)
+            
 
-
+    print(f"Generated {total_generated_slabs} slabs")
 if __name__ == "__main__":
     main()
